6469d71f31e36bf128ab7a23d868326b5635be9b
arsenii <arsenij.arxipov.1998@gmail.com>
2025-04-05 12:28:42 +0300

    fix seeder service

--- src/prisma/prisma-seeder.service.ts
+++ src/prisma/prisma-seeder.service.ts
@@ -11,22 +11,22 @@ export class PrismaSeederService {
     const todos = [
       {
         id: '550e8400-e29b-41d4-a716-446655440000',
-        title: 'clear bath',
-        description: 'do deep clean',
+        title: 'Clean bathroom',
+        description: 'Do a deep clean',
         status: false,
         dateCreate: 1743682445871,
       },
       {
         id: '6ba7b810-9dad-11d1-80b4-00c04fd430c8',
-        title: 'go shopping',
-        description: 'buy shirts',
+        title: 'Go shopping',
+        description: 'Buy shirts',
         status: false,
         dateCreate: 1743682447871,
       },
       {
         id: '8f7c2a3d-5b9e-4f1a-b8c9-d2e4f5678901',
-        title: 'prepare dinner',
-        description: 'I want vegetable curry',
+        title: 'Prepare dinner',
+        description: 'I want to make vegetable curry',
         status: true,
         dateCreate: 1743682417871,
       },
